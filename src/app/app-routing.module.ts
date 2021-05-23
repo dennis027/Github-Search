2c277999d0b9f5e0ce3361d86c5ea2a69add58a6
Dennis <dennis.kimani@student.moringaschool.com>
2021-05-23 19:31:44 +0300

    creating module logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitFormComponent } from './git-form/git-form.component';
+import { GitResultComponent } from './git-result/git-result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'gitForm' , component : GitFormComponent },
+  {path : 'gitResult', component :GitResultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
