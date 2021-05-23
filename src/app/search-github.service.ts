4253b580a3cad45bc6addc43d2e7aa3b3abaa349
Dennis <dennis.kimani@student.moringaschool.com>
2021-05-24 00:13:52 +0300

    adding get service method

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -11,6 +11,32 @@ export class SearchGithubService {
  repository!: Repository;
  repoData =[]
   constructor(private http: HttpClient) {
-    this.user = new User("",0,"","",new Date);
+    this.user = new User("",0,"","",new Date());
+    this.repository= new Repository( "","",new Date(),"","");
+   }
+
+   getUserData(usernames:string){
+     interface ApiResonse {
+       bio : string,
+       public_repos : number,
+       login : string,
+       avator_url : string,
+       created_at :  Date,
+     }
+     
+   
+     let promise = new Promise <void>((resolve,reject)=>{
+      this.http.get<ApiResponse>("https://api.github.com/users/" + usernames).toPromise().then (response=>{
+        this.user.bio = response.bio;
+        this.user.public_repos = response.public_repos;
+        this.user.login = response.login;
+        this.user.avator_url = response.avatar_url;
+        this.user.created_at = response.created_at;
+       })
+      
+       
+       resolve()
+       
+     })
    }
 }
